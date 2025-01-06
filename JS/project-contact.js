953f87b1d8310b191088d84fb7d7c06bf900ee6f
Muhammad Fikri Haziq <mfhzaki@gmail.com>
2025-01-07 01:06:44 +0800

    Update project-contact.js

--- JS/project-contact.js
+++ JS/project-contact.js
@@ -1,4 +1,13 @@
-    document.querySelector('form').addEventListener('submit', function(event) {
+function toggleMenu() {
+    var menuElement = document.getElementById("header-right-menu");
+    if (menuElement.style.display === "block") {
+        menuElement.style.display = "none";
+    } else {
+        menuElement.style.display = "block";
+    }
+}
+
+document.querySelector('form').addEventListener('submit', function(event) {
 event.preventDefault(); // Prevent the form from submitting
 alert('Your message has been submitted!');
 });
